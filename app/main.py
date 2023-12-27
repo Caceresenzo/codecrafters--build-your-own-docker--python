62559e2d96f44144028a4eb55e83f2a058bfa019
Caceresenzo <caceresenzo1502@gmail.com>
2023-12-27 01:26:18 +0100

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -6,8 +6,13 @@ def main():
     image = sys.argv[2]
     argv = sys.argv[3:]
 
-    os.execv(argv[0], argv)
+    pid = os.fork()
+    if not pid:
+        os.execv(argv[0], argv)
 
+    _, status = os.waitpid(pid, 0)
+    exit_code = os.WEXITSTATUS(status)
+    sys.exit(exit_code)
 
 if __name__ == "__main__":
     main()
